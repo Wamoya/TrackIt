ba1745f80dcc8f71f8c5be71c0c53f3e960bb505
Wamoya <juangrossodll@gmail.com>
2025-05-05 15:34:22 +0200

    Create separate file for auxiliary routines

---
+++ src/routines.py
@@ -0,0 +1,15 @@
+import modules.ui as ui
+import modules.readers as readers
+from modules.readers import Objective, Log
+
+
+def read_db(objectives_path, log_path) -> tuple[list[Objective], list[Log]]: # Read both .csv files and return their values
+    print(ui.color("green") + "\tReloading..." + ui.color("reset"))
+
+    objectives = readers.read_objectives(objectives_path)
+    print(f"\t- {len(objectives)} objective(s) were loaded.")
+
+    logs = readers.read_logs(log_path)
+    print(f"\t- {len(logs)} log(s) were loaded.\n")
+
+    return objectives, logs
